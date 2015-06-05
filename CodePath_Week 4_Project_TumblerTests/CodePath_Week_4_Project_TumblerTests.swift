37e7deb5c5ca2e7a216ba86a5d77203bc3e7ed5d
chieger <charleshieger@Charless-MacBook.local>
2015-06-05 12:54:18 -0700

    Initial Commit

---
+++ CodePath_Week 4_Project_TumblerTests/CodePath_Week_4_Project_TumblerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CodePath_Week_4_Project_TumblerTests.swift
+//  CodePath_Week 4_Project_TumblerTests
+//
+//  Created by Charles Hieger on 6/5/15.
+//  Copyright (c) 2015 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CodePath_Week_4_Project_TumblerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
